fcd8ab2d85a9a9733767c4d1b03fe7a19beec478
Glitch (glitch-hello-website) <none>
2022-12-08 21:55:28 +0000

    🎅🏨 Checkpoint ./js/sounds.js:32914181/1196

--- js/sounds.js
+++ js/sounds.js
@@ -1,16 +1,14 @@
 //Sounds
 var sounds = {
-  sounds: {
+  _sounds: {
     1: {url: 'audio/1.mp3'},
   },
   init: function () {
-    for (let id in this.sounds) {
-      this.setupSound(this.sounds[id], id);
+    for (let id in this._sounds) {
+      this._setupSound(this._sounds[id], id);
     }
-    
-    console.log(this.sounds);
   },
-  setupSound: function (sound, id) {
+  _setupSound: function (sound, id) {
     sound.channels = [];
     sound.index = 0;
     sound.volume = 1;
@@ -18,5 +16,37 @@ var sounds = {
     for (let i = 0; i < 5; i++) {
       sound.channels.push(new Audio(sound.url));
     }
+  },
+  _getSound: function (id) {
+    return this._sounds[id];
+  },
+  _getSoundChannel: function (id, index) {
+    let sound = this._getSound(id);
+
+    index = typeof index === 'undefined' ? sound.index : index;
+
+    let audio = sound.channels[index];
+
+    audio.volume = sound.volume;
+
+    return audio;
+  },
+  _cycleSound: function (id) {
+    let sound = this._getSound(id);
+
+    sound.index++;
+    sound.index = sound.index < sound.num ? sound.index : 0;
+
+    sound.volume = 1; //Reset volume, ready for next sound
+  },
+  _playSound: function (id) {
+    let audio = this._getSoundChannel(id);
+
+    audio.play();
+
+    this._cycleSound(id);
+  },
+  play: function (id) {
+    this._playSound(id);
   },
 };

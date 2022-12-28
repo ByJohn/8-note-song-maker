49580663b47509e6e37d2a1616197d4a1863fb23
Glitch (8-note-song-maker) <none>
2022-12-28 11:34:31 +0000

    🌷💅 Checkpoint ./js/sidebars.js:32914181/618 ./css/style.css:32914181/18

--- js/sidebars.js
+++ js/sidebars.js
@@ -1,6 +1,27 @@
 //Sidebars
 var sidebars = {
-  
+  $menuToggle: document.getElementById('menu-toggle'),
   sidebars: {},
-  init: function () {},
+
+  init: function () {
+    let that = this;
+
+    document.querySelectortAll('.sidebar[data-sidebar]').forEach(function ($sidebar) {
+      let name = $sidebar.dataset.sidebar;
+
+      that.sidebars[name] = $sidebar;
+    });
+
+    this.setupEvents();
+  },
+  setupEvents: function () {
+    this.$menuToggle.addEventListener('click', this.openMenu.bind(this), false);
+  },
+
+  openMenu: function () {
+    this.open('menu');
+  },
+
+  open: function (name) {},
+  close: function (name) {},
 };
